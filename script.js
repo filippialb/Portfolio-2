914bf012aa46210f5957d36c6c2a5eb1527802d6
Albuquerque <filippialbuquerque@gmail.com>
2023-10-16 13:07:54 -0300

    portfolio

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+
+
 function scrollToElement(elementSelector, instance = 0){
     //Select all elements that match the given selector
     const elements = document.querySelectorAll(elementSelector);
@@ -8,20 +12,77 @@ function scrollToElement(elementSelector, instance = 0){
     }
 }
 
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
 
-link1.addEventListener('click', () => {
+
+
+
+
+const linkJobDone = document.getElementById("linkJobDone");
+const linkPricing = document.getElementById("linkPricing");
+const linkAbout = document.getElementById("linkAbout");
+const linkGetInTouch = document.getElementById("linkGetInTouch");
+const linkGetInTouch2 = document.getElementById("linkGetInTouch2");
+
+linkJobDone.addEventListener('click', () => {
      scrollToElement('.header');
 });
 
-link2.addEventListener('click', () => {
+
+linkPricing.addEventListener('click', () => {
     //scrool to the second element with "header" class 
     scrollToElement('.header', 1);
 });
 
-link3.addEventListener('click', () => {
+linkAbout.addEventListener('click', () => {
     scrollToElement('.column');
 });
+
+linkGetInTouch.addEventListener('click', () => {
+  scrollToElement('.header', 2);
+});
+
+linkGetInTouch2.addEventListener('click', () => {
+  scrollToElement('.header', 2);
+});
+
+
+
+
+
+emailjs.init("JcV5iPHNv8AMgB7Hq");
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    // Check if any of the fields are empty
+    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+      alert('Please fill out all fields.');
+      return;
+    }
+  
+    // Send the email using EmailJS
+    emailjs.send("service_957rz0n", "template_7a4muam", {
+      name: name,
+      email: email,
+      message: message
+    }).then(function(response) {
+      alert('Message sent successfully!');
+      // clear the form after sending the message
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('message').value = '';
+    }, function(error) {
+      console.error('Error sending message:', error);
+      alert('Failed to send message. Please try again later.');
+    });
+  });
+
+  
+
+
+
+
 
